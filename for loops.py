0a66ce3f4cc41295fec171648a48faf9fd91187d
NR86 <rajn29821@gmail.com>
2026-09-10 14:33:10 +0530

    index number

--- for loops.py
+++ for loops.py
@@ -1,3 +1,13 @@
-#numbers 1-10 using for loop
-for i in range(1, 11):
-    print(i)
+# #numbers 1-10 using for loop
+# for i in range(1, 11):
+#     print(i)
+
+# #how to find which number is on which index
+l1=[45, 67, 89, 90, 56, 78]
+target=90
+for i in range(len(l1)):
+    if l1[i] == target:
+        print("Found at index:", i)
+        break
+else:
+    print("Target not found.")

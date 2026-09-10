1e46018fa4f620e64a72aa7669d58d233d42729e
NR86 <rajn29821@gmail.com>
2026-09-10 14:36:02 +0530

    even numbers from 1-50

--- for loops.py
+++ for loops.py
@@ -3,11 +3,17 @@
 #     print(i)
 
 # #how to find which number is on which index
-l1=[45, 67, 89, 90, 56, 78]
-target=90
-for i in range(len(l1)):
-    if l1[i] == target:
-        print("Found at index:", i)
-        break
-else:
-    print("Target not found.")
+# l1=[45, 67, 89, 90, 56, 78]
+# target=90
+# for i in range(len(l1)):
+#     if l1[i] == target:
+#         print("Found at index:", i)
+#         break
+# else:
+#     print("Target not found.")
+
+#Even numbers from 1-50
+for i in range(1,51):
+    if i%2==0:
+        print(i)
+    

02770a203910ab910636e525f6a07e04992df7e2
NR86 <rajn29821@gmail.com>
2026-09-10 14:23:19 +0530

    calculating average

--- while.py
+++ while.py
@@ -1,6 +1,14 @@
-#Even or odd number#
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print("The number is even.")
-else:
-    print("The number is odd.")
+# #Even or odd number#
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+marks=[67, 45, 89, 90, 56, 78]
+total=0
+for mark in marks:
+    total += mark
+
+average = total / len(marks)
+print("The average marks is:", average)
